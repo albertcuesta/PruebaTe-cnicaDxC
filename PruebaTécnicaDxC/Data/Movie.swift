a8b655b9c2b768150d1b2686f27a6fe649bc03a2
albertcuesta <acm2291@gmail.com>
2021-05-26 12:01:30 +0200

    Creamos structura de la pelicula

---
+++ PruebaTécnicaDxC/Data/Movie.swift
@@ -0,0 +1,26 @@
+//
+//  Movie.swift
+//  PruebaTécnicaDxC
+//
+//  Created by Albert on 26/5/21.
+//
+
+import Foundation
+import SwiftUI
+
+struct Movie {
+    var id: Int
+    var original_title: String
+    var title: String
+    var original_language: String
+    var adult: Bool
+    var overview: String
+    var release_date: Date
+    var vote_count: Int
+    var video: Bool
+    var vote_average: Int
+    var backdrop_path: Image
+    
+    
+    
+}
